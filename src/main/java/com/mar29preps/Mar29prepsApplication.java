8fa1278a0442ca568f6305baed29750568716948
Matt Cadorette <mcadorette@gmail.com>
2019-03-29 15:30:15 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/mar29preps/Mar29prepsApplication.java
@@ -0,0 +1,12 @@
+package com.mar29preps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mar29prepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mar29prepsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mar29preps/Mar29prepsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mar29preps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mar29prepsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
